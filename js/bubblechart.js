ec45286ad532e7715981510dc2b1e1f6dd1c0ea7
Shuo Li <li3shuo1@gmail.com>
2020-12-03 00:13:34 -0500

    fixed bubble chart brush issue by moving updateChart() to endBrush()

--- js/bubblechart.js
+++ js/bubblechart.js
@@ -213,7 +213,7 @@ function drawChart(route_info, GPS_routes) {
         [padding.l, 0],
         [chartWidth, chartHeight],
       ])
-      .on("start brush", updateChart)
+      //.on("start brush", updateChart)
       .on("end", brushend)
   );
   //Adding size for cost
@@ -366,6 +366,7 @@ function drawChart(route_info, GPS_routes) {
     ); // This return TRUE or FALSE depending on if the points is in the selected area
   }
   function brushend() {
+    updateChart();
     if (!d3.event.selection) {
       polylines.removeFrom(map2);
     }
